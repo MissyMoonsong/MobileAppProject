dfd57ad67c6931fcaee54b325607d4ab002cbd63
jewel.siren <jewel.siren@yahoo.com>
2017-03-27 15:51:13 -0400

    Added code (unused) from the other database attempt.

--- app/src/main/java/com/example/jewel/test_project/DataManager.java
+++ app/src/main/java/com/example/jewel/test_project/DataManager.java
@@ -45,7 +45,23 @@ public class DataManager {
     private Map<String, Group> groups;
 
     private DataManager() {
-        createDummySchedule();
+        createDummySchedule(); //TODO: Remove this when database works
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().getRoot();
+        ref.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //refreshFromDatabase(dataSnapshot);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                }
+        );
+
     }
 
     public static DataManager Instance() {
@@ -168,5 +184,130 @@ public class DataManager {
         String val = Integer.toString(nextUserID);
         nextUserID++;
         return val;
+    }
+
+    public void refreshFromDatabase(DataSnapshot dataSnapshot){
+        groups = new HashMap<>();
+
+        //Creating firebase object
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        //Get user id
+        String userID = user.getUid();
+        String name =  user.getEmail();
+        //Set up user object
+        this.user = new Person(name, userID);
+
+        Schedule userSchedule = this.user.getSchedule();
+        buildSchedule(userSchedule, userID, dataSnapshot);
+
+        //get all groups user is in
+        DataSnapshot userGroups = dataSnapshot.child("MembershipUserToGroup").child(userID);
+        //For reach group
+        for(DataSnapshot groupIDSnap : userGroups.getChildren()){
+            String groupID = groupIDSnap.getValue().toString();
+
+            //Get group name
+            DataSnapshot groupSnap = dataSnapshot.child("Group").child(groupID);
+            String groupName = groupSnap.getValue().toString();
+
+            Group g = new Group(groupName, groupID);
+            groups.put(groupID, g);
+
+            //for each member
+            DataSnapshot groupMembers = dataSnapshot.child("MembershipGroupToUser").child(groupID);
+
+            for(DataSnapshot memberIDSnap : groupMembers.getChildren()){
+                String memberID = memberIDSnap.getValue().toString();
+
+                buildSchedule(g.getGroupSchedule(), memberID, dataSnapshot);
+            }
+        }
+    }
+
+    public void buildSchedule(Schedule s, String userID, DataSnapshot dataSnapshot){
+        //get all events user connects to -- build schedule
+        DataSnapshot schedules = dataSnapshot.child("Schedule");
+        DataSnapshot userSchedule = schedules.child(userID);
+
+        for(DataSnapshot eventIDChild : userSchedule.getChildren()){
+            String eventID = eventIDChild.getValue().toString();
+
+            DataSnapshot eventSnapshot = dataSnapshot.child("Event");
+            DataSnapshot event_id_snap = eventSnapshot.child(eventID);
+
+            Map<String, Object> attributes = new HashMap<>();
+
+            //Make a map of this event's attributes
+            for(DataSnapshot eventAttribute : event_id_snap.getChildren()) {
+                attributes.put(eventAttribute.getKey(), eventAttribute.getValue());
+            }
+
+            ScheduleEvent e = buildEventFromMap(attributes);
+            e.setEventID(eventID);
+
+            s.addEvent(e);
+        }
+    }
+
+    public ScheduleEvent buildEventFromMap(Map<String, Object> attributes){
+        String eName = attributes.get("eventName").toString();
+
+        int sYear = Integer.parseInt(attributes.get("startYear").toString());
+        int sMonth = Integer.parseInt(attributes.get("startMonth").toString());
+        int sDay = Integer.parseInt(attributes.get("startDay").toString());
+        int sHour = Integer.parseInt(attributes.get("startHour").toString());
+        int sMin = Integer.parseInt(attributes.get("startMin").toString());
+
+        int eYear  = Integer.parseInt(attributes.get("endYear").toString());
+        int eMonth  = Integer.parseInt(attributes.get("endMonth").toString());
+        int eDay  = Integer.parseInt(attributes.get("endDay").toString());
+        int eHour  = Integer.parseInt(attributes.get("endHour").toString());
+        int eMin  = Integer.parseInt(attributes.get("endMin").toString());
+
+        boolean sun = (boolean)attributes.get("rsunday");
+        boolean mon= (boolean)attributes.get("rmonday");
+        boolean tue= (boolean)attributes.get("rtuesday");
+        boolean wed= (boolean)attributes.get("rwednesday");
+        boolean thu= (boolean)attributes.get("rthursday");
+        boolean fri= (boolean)attributes.get("rfriday");
+        boolean sat= (boolean)attributes.get("rsaturday");
+
+        //Adding to the schedule within the app
+        Calendar start = Calendar.getInstance();
+
+        start.set(Calendar.YEAR, sYear);
+        start.set(Calendar.MONTH, sMonth);
+        start.set(Calendar.DAY_OF_MONTH, sDay);
+        start.set(Calendar.HOUR_OF_DAY, sHour);
+        start.set(Calendar.MINUTE, sMin);
+
+        Calendar end = Calendar.getInstance();
+        end.set(Calendar.YEAR, eYear);
+        end.set(Calendar.MONTH, eMonth);
+        end.set(Calendar.DAY_OF_MONTH, eDay);
+        end.set(Calendar.HOUR_OF_DAY, eHour);
+        end.set(Calendar.MINUTE, eMin);
+
+        boolean[] weekdays = new boolean[7];
+        weekdays[0] = sun;
+        weekdays[1] = mon;
+        weekdays[2] = tue;
+        weekdays[3] = wed;
+        weekdays[4] = thu;
+        weekdays[5] = fri;
+        weekdays[6] = sat;
+
+
+        ScheduleEvent e;
+
+        if(ScheduleEvent.anyDaySelected(weekdays)){ //Recurring
+            e = new ScheduleEvent(eName, start, end, weekdays);
+
+        } else{
+            e = new ScheduleEvent(eName, start, end);
+        }
+
+        return e;
     }
 }
