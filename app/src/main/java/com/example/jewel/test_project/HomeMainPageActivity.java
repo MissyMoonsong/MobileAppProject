7532a4301f7c7049c3ff7fae299795e0d27bd46c
Gwen Stevens <stevens.826@osu.edu>
2017-04-04 18:55:10 -0400

    Working on making a home page, so Home button doesn't send you back to login

---
+++ app/src/main/java/com/example/jewel/test_project/HomeMainPageActivity.java
@@ -0,0 +1,194 @@
+package com.example.jewel.test_project;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.client.Firebase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jewel on 3/26/2017.
+ * This class is being adapted from work I did in a previous app,
+ * which was designed to display passwords. Any leftover references
+ * to passwords are accidental
+ */
+
+public class HomeMainPageActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    //Buttons
+    Button btnAdd;
+
+    //for the menu
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Firebase.setAndroidContext(this);
+
+
+        btnAdd = (Button) findViewById(R.id.btn_groupPage);
+        btnAdd.setOnClickListener(this);
+
+        //menu
+        mDrawerList = (ListView) findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+        mDrawerList.bringToFront();
+        mDrawerLayout.requestLayout();
+        addDrawerItems();
+        setupDrawer();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    //For menu
+    private void addDrawerItems() {
+        mAdapter = DrawerData.makeDrawerAdapter(this);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(HomeMainPageActivity.this, DrawerData.classArray[position]);
+                intent.putExtras(DrawerData.makeBundleForEventView());
+                startActivity(intent);
+            }
+        });
+    }
+
+    //for menu
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.openDrawer, R.string.closeDrawer) {
+
+            //menu is open
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Main-Nav");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            //menu is closed
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    //Helper method because the adapter needs an array of PasswordEntry, not array list
+    private Group[] makeArray(List<Group> list) {
+        Group[] array = new Group[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
+
+        return array;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //Refresh the list view
+
+
+
+        mDrawerList = (ListView) findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+        mDrawerList.bringToFront();
+        mDrawerLayout.requestLayout();
+        addDrawerItems();
+        setupDrawer();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnAdd) {
+            Intent intent = new Intent(this, GroupMainPageActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        //End activity if the user leaves the app
+        super.onPause();
+        finish();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    //menu
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
