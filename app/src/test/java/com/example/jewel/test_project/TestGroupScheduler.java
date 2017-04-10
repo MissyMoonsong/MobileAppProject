775053e4f2e18976aaac096da554238135a27eaf
jewel.siren <jewel.siren@yahoo.com>
2017-04-10 19:40:09 -0400

    Added tests cases for the ScheduleBlock.

--- app/src/test/java/com/example/jewel/test_project/TestGroupScheduler.java
+++ app/src/test/java/com/example/jewel/test_project/TestGroupScheduler.java
@@ -8,6 +8,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.junit.Test;
 
+import java.util.Calendar;
+
 import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 import static org.junit.Assert.*;
 
@@ -72,6 +74,274 @@ public class TestGroupScheduler {
         assertEquals("Dinner", scheduleEvent.getEventName());
         assertEquals("Time: 03:00 AM to 03:55 AM", scheduleEvent.getEventTime());
         assertEquals("One Time Event", scheduleEvent.getEventRecurrence());
+    }
+
+    @Test
+    public void ScheduleEvent_createBlockInDay() throws Exception {
+        Calendar day = Calendar.getInstance();
+
+        Calendar eventStart = Calendar.getInstance();
+        eventStart.set(Calendar.MONTH, 0);
+        eventStart.set(Calendar.DAY_OF_MONTH, 1);
+        eventStart.set(Calendar.HOUR_OF_DAY, 3);
+        eventStart.set(Calendar.MINUTE, 0);
+
+        Calendar eventEnd = Calendar.getInstance();
+        eventEnd.set(Calendar.MONTH, 0);
+        eventEnd.set(Calendar.DAY_OF_MONTH, 1);
+        eventEnd.set(Calendar.HOUR_OF_DAY, 3);
+        eventEnd.set(Calendar.MINUTE, 55);
+
+        boolean[] weekdays = new boolean[] {true, true, true, true, true, true, true};
+
+        ScheduleEvent scheduleEvent = new ScheduleEvent("Intersections", eventStart, eventEnd, weekdays);
+
+        CalendarDay today = new CalendarDay(day);
+
+        scheduleEvent.generateBlockOnDay(today);
+
+        ScheduleBlock b = today.getBlocksInDay().get(0);
+
+        assertEquals(1,today.getBlocksInDay().size());
+        assertEquals(scheduleEvent, b.getParentEvent());
+    }
+
+    @Test
+    public void ScheduleBlock_intersectsWith_startsBefore() throws Exception {
+        Calendar day = Calendar.getInstance();
+
+        Calendar eventStart = Calendar.getInstance();
+        eventStart.set(Calendar.MONTH, 0);
+        eventStart.set(Calendar.DAY_OF_MONTH, 1);
+        eventStart.set(Calendar.HOUR_OF_DAY, 3);
+        eventStart.set(Calendar.MINUTE, 0);
+
+        Calendar eventEnd = Calendar.getInstance();
+        eventEnd.set(Calendar.MONTH, 0);
+        eventEnd.set(Calendar.DAY_OF_MONTH, 1);
+        eventEnd.set(Calendar.HOUR_OF_DAY, 3);
+        eventEnd.set(Calendar.MINUTE, 55);
+
+        boolean[] weekdays = new boolean[] {true, true, true, true, true, true, true};
+
+        ScheduleEvent scheduleEvent = new ScheduleEvent("Intersections", eventStart, eventEnd, weekdays);
+
+        CalendarDay today = new CalendarDay(day);
+
+        scheduleEvent.generateBlockOnDay(today);
+
+        ScheduleBlock b = today.getBlocksInDay().get(0);
+
+        //Note that the above event takes place at 3 am to 3:55 am each day
+        Calendar start = Calendar.getInstance();
+        Calendar end = Calendar.getInstance();
+
+        //This window starts before the block and ends in the middle of it
+        start.set(Calendar.HOUR_OF_DAY, 2);
+        end.set(Calendar.HOUR_OF_DAY, 3);
+        end.set(Calendar.MINUTE, 30);
+
+        boolean intersection =  b.intersectsWith(start, end);
+        assertEquals(true,intersection);
+    }
+
+    @Test
+    public void ScheduleBlock_intersectsWith_endsAfter() throws Exception {
+        Calendar day = Calendar.getInstance();
+
+        Calendar eventStart = Calendar.getInstance();
+        eventStart.set(Calendar.MONTH, 0);
+        eventStart.set(Calendar.DAY_OF_MONTH, 1);
+        eventStart.set(Calendar.HOUR_OF_DAY, 3);
+        eventStart.set(Calendar.MINUTE, 0);
+
+        Calendar eventEnd = Calendar.getInstance();
+        eventEnd.set(Calendar.MONTH, 0);
+        eventEnd.set(Calendar.DAY_OF_MONTH, 1);
+        eventEnd.set(Calendar.HOUR_OF_DAY, 3);
+        eventEnd.set(Calendar.MINUTE, 55);
+
+        boolean[] weekdays = new boolean[] {true, true, true, true, true, true, true};
+
+        ScheduleEvent scheduleEvent = new ScheduleEvent("Intersections", eventStart, eventEnd, weekdays);
+
+        CalendarDay today = new CalendarDay(day);
+
+        scheduleEvent.generateBlockOnDay(today);
+
+        ScheduleBlock b = today.getBlocksInDay().get(0);
+
+        //Note that the above event takes place at 3 am to 3:55 am each day
+        Calendar start = Calendar.getInstance();
+        Calendar end = Calendar.getInstance();
+
+        //This window starts inside the block and ends after it
+        start.set(Calendar.HOUR_OF_DAY, 3);
+        start.set(Calendar.MINUTE, 30);
+        end.set(Calendar.HOUR_OF_DAY, 5);
+
+        boolean intersection =  b.intersectsWith(start, end);
+        assertEquals(true,intersection);
+    }
+
+    @Test
+    public void ScheduleBlock_intersectsWith_withinBlock() throws Exception {
+        Calendar day = Calendar.getInstance();
+
+        Calendar eventStart = Calendar.getInstance();
+        eventStart.set(Calendar.MONTH, 0);
+        eventStart.set(Calendar.DAY_OF_MONTH, 1);
+        eventStart.set(Calendar.HOUR_OF_DAY, 3);
+        eventStart.set(Calendar.MINUTE, 0);
+
+        Calendar eventEnd = Calendar.getInstance();
+        eventEnd.set(Calendar.MONTH, 0);
+        eventEnd.set(Calendar.DAY_OF_MONTH, 1);
+        eventEnd.set(Calendar.HOUR_OF_DAY, 3);
+        eventEnd.set(Calendar.MINUTE, 55);
+
+        boolean[] weekdays = new boolean[] {true, true, true, true, true, true, true};
+
+        ScheduleEvent scheduleEvent = new ScheduleEvent("Intersections", eventStart, eventEnd, weekdays);
+
+        CalendarDay today = new CalendarDay(day);
+
+        scheduleEvent.generateBlockOnDay(today);
+
+        ScheduleBlock b = today.getBlocksInDay().get(0);
+
+        //Note that the above event takes place at 3 am to 3:55 am each day
+        Calendar start = Calendar.getInstance();
+        Calendar end = Calendar.getInstance();
+
+        //This window starts inside the block and ends inside it
+        start.set(Calendar.HOUR_OF_DAY, 3);
+        start.set(Calendar.MINUTE, 5);
+        end.set(Calendar.HOUR_OF_DAY, 3);
+        end.set(Calendar.MINUTE, 30);
+
+        boolean intersection =  b.intersectsWith(start, end);
+        assertEquals(true,intersection);
+    }
+
+    @Test
+    public void ScheduleBlock_intersectsWith_containsBlock() throws Exception {
+        Calendar day = Calendar.getInstance();
+
+        Calendar eventStart = Calendar.getInstance();
+        eventStart.set(Calendar.MONTH, 0);
+        eventStart.set(Calendar.DAY_OF_MONTH, 1);
+        eventStart.set(Calendar.HOUR_OF_DAY, 3);
+        eventStart.set(Calendar.MINUTE, 0);
+
+        Calendar eventEnd = Calendar.getInstance();
+        eventEnd.set(Calendar.MONTH, 0);
+        eventEnd.set(Calendar.DAY_OF_MONTH, 1);
+        eventEnd.set(Calendar.HOUR_OF_DAY, 3);
+        eventEnd.set(Calendar.MINUTE, 55);
+
+        boolean[] weekdays = new boolean[] {true, true, true, true, true, true, true};
+
+        ScheduleEvent scheduleEvent = new ScheduleEvent("Intersections", eventStart, eventEnd, weekdays);
+
+        CalendarDay today = new CalendarDay(day);
+
+        scheduleEvent.generateBlockOnDay(today);
+
+        ScheduleBlock b = today.getBlocksInDay().get(0);
+
+        //Note that the above event takes place at 3 am to 3:55 am each day
+        Calendar start = Calendar.getInstance();
+        Calendar end = Calendar.getInstance();
+
+        //This window starts before the block and ends after it
+        start.set(Calendar.HOUR_OF_DAY, 2);
+        start.set(Calendar.MINUTE, 5);
+        end.set(Calendar.HOUR_OF_DAY, 5);
+        end.set(Calendar.MINUTE, 30);
+
+        boolean intersection =  b.intersectsWith(start, end);
+        assertEquals(true,intersection);
+    }
+
+    @Test
+    public void ScheduleBlock_intersectsWith_beforeBlock() throws Exception {
+        Calendar day = Calendar.getInstance();
+
+        Calendar eventStart = Calendar.getInstance();
+        eventStart.set(Calendar.MONTH, 0);
+        eventStart.set(Calendar.DAY_OF_MONTH, 1);
+        eventStart.set(Calendar.HOUR_OF_DAY, 3);
+        eventStart.set(Calendar.MINUTE, 0);
+
+        Calendar eventEnd = Calendar.getInstance();
+        eventEnd.set(Calendar.MONTH, 0);
+        eventEnd.set(Calendar.DAY_OF_MONTH, 1);
+        eventEnd.set(Calendar.HOUR_OF_DAY, 3);
+        eventEnd.set(Calendar.MINUTE, 55);
+
+        boolean[] weekdays = new boolean[] {true, true, true, true, true, true, true};
+
+        ScheduleEvent scheduleEvent = new ScheduleEvent("Intersections", eventStart, eventEnd, weekdays);
+
+        CalendarDay today = new CalendarDay(day);
+
+        scheduleEvent.generateBlockOnDay(today);
+
+        ScheduleBlock b = today.getBlocksInDay().get(0);
+
+        //Note that the above event takes place at 3 am to 3:55 am each day
+        Calendar start = Calendar.getInstance();
+        Calendar end = Calendar.getInstance();
+
+        //This window starts before the block and ends before it
+        start.set(Calendar.HOUR_OF_DAY, 2);
+        start.set(Calendar.MINUTE, 5);
+        end.set(Calendar.HOUR_OF_DAY, 2);
+        end.set(Calendar.MINUTE, 30);
+
+        boolean intersection =  b.intersectsWith(start, end);
+        assertEquals(false ,intersection);
+    }
+
+    @Test
+    public void ScheduleBlock_intersectsWith_afterBlock() throws Exception {
+        Calendar day = Calendar.getInstance();
+
+        Calendar eventStart = Calendar.getInstance();
+        eventStart.set(Calendar.MONTH, 0);
+        eventStart.set(Calendar.DAY_OF_MONTH, 1);
+        eventStart.set(Calendar.HOUR_OF_DAY, 3);
+        eventStart.set(Calendar.MINUTE, 0);
+
+        Calendar eventEnd = Calendar.getInstance();
+        eventEnd.set(Calendar.MONTH, 0);
+        eventEnd.set(Calendar.DAY_OF_MONTH, 1);
+        eventEnd.set(Calendar.HOUR_OF_DAY, 3);
+        eventEnd.set(Calendar.MINUTE, 55);
+
+        boolean[] weekdays = new boolean[] {true, true, true, true, true, true, true};
+
+        ScheduleEvent scheduleEvent = new ScheduleEvent("Intersections", eventStart, eventEnd, weekdays);
+
+        CalendarDay today = new CalendarDay(day);
+
+        scheduleEvent.generateBlockOnDay(today);
+
+        ScheduleBlock b = today.getBlocksInDay().get(0);
+
+        //Note that the above event takes place at 3 am to 3:55 am each day
+        Calendar start = Calendar.getInstance();
+        Calendar end = Calendar.getInstance();
+
+        //This window starts after the block and ends after it
+        start.set(Calendar.HOUR_OF_DAY, 4);
+        start.set(Calendar.MINUTE, 0);
+        end.set(Calendar.HOUR_OF_DAY, 5);
+        end.set(Calendar.MINUTE, 30);
+
+        boolean intersection =  b.intersectsWith(start, end);
+        assertEquals(false,intersection);
     }
 }
 
